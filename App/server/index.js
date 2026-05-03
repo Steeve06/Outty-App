9fff5f3ed9bbe9099c4c1382c1b907637bdeef14
Tobechukwu Kennedy Eziolise <tobechukwueziolise@gmail.com>
2026-05-03 16:56:26 -0400

    Updated cors configuration

--- App/server/index.js
+++ App/server/index.js
@@ -3,7 +3,9 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: ['http://localhost:8081', 'https://outty-app-yyq6.vercel.app/']
+}));
 app.use(express.json());
 
 // @ts-ignore
@@ -13,8 +15,5 @@ const photoRoute = require('./src/routes/photoRoutes');
 
 app.use('/api/profile', profileRoute);
 app.use('/api/photos', photoRoute);
-app.use(cors({
-    origin: ['http://localhost:8081', 'https://outty-app-yyq6.vercel.app/']
-}));
 
 module.exports = app;
